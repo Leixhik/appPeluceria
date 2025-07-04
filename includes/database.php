b021b3d15bc999d38f7a46f84153b755a62ca7f4
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-07-04 16:29:19 -0600

    creacion de database + modificacion de idioma en index + config de php

--- includes/database.php
+++ includes/database.php
@@ -1,2 +1,14 @@
 <?php
 
+require_once('config.php');
+
+$db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if (!$db){
+    die('Error de conexión: ' . mysqli_connect_error());
+}
+
+echo "<pre>";
+var_dump($db);
+echo "</pre>";
+
